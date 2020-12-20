cbc9e8753235c0f5c46585e6b2764cc382cdc1e3
742359579@qq.com <mf1923078>
2020-12-20 19:06:05 +0800

    udp socket

---
+++ udpsocket/client.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#define SERV_PORT 9998
+//udp client
+int main()
+{
+    int sockfd;
+    char buf[BUFSIZ];
+    char str[INET_ADDRSTRLEN];
+    int i, n;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in serv_addr, clit_addr;
+    bzero(&serv_addr, sizeof(serv_addr)); //将结构体清零
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERV_PORT);
+    //serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //自
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    while (fgets(buf,BUFSIZ,stdin)!=NULL)
+    {
+
+        n = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+        if (n == -1)
+            perror("sendto error");
+        //n = recvfrom(sockfd, buf, BUFSIZ, 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+        n = recvfrom(sockfd, buf, BUFSIZ, 0, NULL,0);//不关心对端信息
+        if (n == -1)
+            perror("recvfrom error");
+
+        write(STDOUT_FILENO,buf,n);
+    }
+    close(sockfd);
+}
---
+++ udpsocket/server.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#define SERV_PORT 9998
+//udp server
+int main()
+{
+    int sockfd;
+    char buf[BUFSIZ];
+    char str[INET_ADDRSTRLEN];
+    int i, n;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in serv_addr, clit_addr;
+    bzero(&serv_addr, sizeof(serv_addr)); //将结构体清零
+    socklen_t clit_addr_len;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERV_PORT);
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //自
+    //int opt=1;
+    //setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,(void*)&opt,sizeof(opt));
+    bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    printf("wait for connecting...\n");
+
+    while (1)
+    {
+        clit_addr_len = sizeof(clit_addr);
+        n = recvfrom(sockfd, buf, BUFSIZ, 0, (struct sockadr *)&clit_addr, &clit_addr_len);
+        if (n == -1)
+        {
+            perror("recvfrom error");
+        }
+        printf("recv from ip:%s,and port:%d \n", inet_ntop(AF_INET, &clit_addr.sin_addr, str, sizeof(str)),
+               ntohs(clit_addr.sin_port));
+        write(STDOUT_FILENO,buf,n);
+        for (i = 0; i < n; i++)
+        {
+            buf[i] = toupper(buf[i]);
+        }
+        n = sendto(sockfd, buf, n, 0, (struct sockaddr *)&clit_addr, sizeof(clit_addr));
+        if (n == -1)
+            perror("sendto error");
+    }
+    close(sockfd);
+}
